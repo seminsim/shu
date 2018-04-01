7d283adcf90e883db62833b263dd3236ae5bbc43
Semin <seminsim@gmail.com>
2018-03-31 17:53:12 -0700

    recover .dll files

---
+++ UnityProject/SHU/Assets/Scripts/Actors/Actor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FlatBuffers;
+using UnityEngine;
+
+namespace Actors
+{
+    public abstract class Actor
+    {
+        private List<IActorObserver> _observers;
+
+        //! feed fake action (networked and not from server) for fake visual representation, logically meaningless
+        public abstract void FeedFakeAction(uint actionId, IFlatbufferObject actionObj);
+        
+        //! feed action (local game or from server), logically meaningful
+        public abstract void FeedAction(uint actionId, IFlatbufferObject actionObj);
+
+        //! simulate a tick (generate game events)
+        public abstract void SimulateTick();
+
+        //! process game events
+        public abstract void SimulateProcessEvent(IEvent ev);
+
+        //! subscribe an observer for an important state change
+        public void Subscribe(IActorObserver observer)
+        {
+            if (_observers.Contains(observer))
+            {
+                Debug.LogError("the observer is already subscribing this actor.");
+                return;
+            }
+            
+            _observers.Add(observer);
+        }
+
+        //! unsubscribe an observer
+        public void Unsubscribe(IActorObserver observer)
+        {
+            _observers = _observers.Where(e => e != observer).ToList();
+        }
+    }
+}
---
+++ UnityProject/SHU/Assets/Scripts/Actors/IEvent.cs
@@ -0,0 +1,5 @@
+﻿namespace Actors
+{
+    public interface IEvent {
+    }
+}
